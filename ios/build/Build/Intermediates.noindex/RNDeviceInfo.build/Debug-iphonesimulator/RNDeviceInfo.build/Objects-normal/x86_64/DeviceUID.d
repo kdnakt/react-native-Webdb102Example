9f4960417eb34f8e50ad4b27cb102c1152e80b7b
kdnakt <a.kid.1985@gmail.com>
2018-01-04 01:20:11 +0900

    remove RNDeviceInfo-tvOS from Link Binary with Libraries

---
+++ ios/build/Build/Intermediates.noindex/RNDeviceInfo.build/Debug-iphonesimulator/RNDeviceInfo.build/Objects-normal/x86_64/DeviceUID.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/akito/Documents/react-native/WebdbReactNativeExample/node_modules/react-native-device-info/RNDeviceInfo/DeviceUID.m \
+  /Users/akito/Documents/react-native/WebdbReactNativeExample/node_modules/react-native-device-info/RNDeviceInfo/DeviceUID.h
---
+++ ios/build/Build/Intermediates.noindex/RNDeviceInfo.build/Debug-iphonesimulator/RNDeviceInfo.build/Objects-normal/x86_64/RNDeviceInfo.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/akito/Documents/react-native/WebdbReactNativeExample/node_modules/react-native-device-info/RNDeviceInfo/RNDeviceInfo.m \
+  /Users/akito/Documents/react-native/WebdbReactNativeExample/node_modules/react-native-device-info/RNDeviceInfo/RNDeviceInfo.h \
+  /Users/akito/Documents/react-native/WebdbReactNativeExample/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
+  /Users/akito/Documents/react-native/WebdbReactNativeExample/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
+  /Users/akito/Documents/react-native/WebdbReactNativeExample/node_modules/react-native-device-info/RNDeviceInfo/DeviceUID.h
